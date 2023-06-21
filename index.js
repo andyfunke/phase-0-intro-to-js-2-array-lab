254e9c792ac0d5f244038dbb5bfe587e7dd7c1c2
Andrew <andrew.i.funke@gmail.com>
2023-06-21 08:14:55 -0700

    Array Methods Lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+console.log(removeLastCat(cats));
+removeFirstCat(cats);
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const AppendedCat = cats.slice();
+    AppendedCat.push(name);
+    return AppendedCat;
+}
+
+function prependCat(name) {
+    const PrependedCat = cats.slice();
+    PrependedCat.unshift(name);
+    return PrependedCat;
+}
+
+function removeLastCat() {
+    const LastCatRemoved = cats.slice();
+    LastCatRemoved.pop();
+    return LastCatRemoved;
+}
+
+function removeFirstCat() {
+    const FirstCatRemoved = cats.slice();
+    FirstCatRemoved.shift();
+    return FirstCatRemoved;
+}
